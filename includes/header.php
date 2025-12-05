2743e48070e14e1fd35d3cd38b6021f9286d8c8f
MathieuL <mathieu.lienard@ynov.com>
2025-12-05 16:28:26 +0100

    fix

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,9 @@ if (!function_exists('isLoggedIn')) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? escape($pageTitle) : 'Aide aux Devoirs'; ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
